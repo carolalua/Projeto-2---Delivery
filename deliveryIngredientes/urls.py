c5af2da52692f6b831c9e8223b8694f5aa813c32
carolalua <57256788+carolalua@users.noreply.github.com>
2021-06-08 17:07:05 -0300

    urls atualizada

--- deliveryIngredientes/urls.py
+++ deliveryIngredientes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 """todas as urls referente a gestãoacademica deverão estar nesse aquivo"""
 urlpatterns = [
- path('', views.listaIngredientes, name='lista-ingredientes'),
- path('cadastrarIngredientes/<int:id>', views.cadastrarIngredientes, name="cadastroI"),
+ path('', views.listar_receitas, name='lista-receitas'),
+ path('detalhes_produto/<int:id>', views.detalhes_produto, name="detalhes"),
 ]
