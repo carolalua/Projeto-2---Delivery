87c2da925095543844f90fd61823633b43225007
carolinesales <81131307+carolinesales@users.noreply.github.com>
2021-06-08 22:40:36 -0300

    Update views.py

--- deliveryIngredientes/views.py
+++ deliveryIngredientes/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Receita
 
 def listar_receitas(request):
-    lista_receita = Receita.objects.filter(disponivel=True)
-    contexto = {
-        'Receita': Receita,
-    }
-    return render(request, 'deliveryIngredientes/index.html', contexto)
+    lista_receita = Receita.objects.all
+
+    return render(request, 'deliveryIngredientes/index.html', {'receitas':lista_receita})
 
 def detalhes_produto(request):
     produto = get_object_or_404(Produto, id=id,disponivel=True)
