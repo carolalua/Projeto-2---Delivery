e165f82ebc01bb76f07d2669e234f5f1d5acdd42
carolalua <57256788+carolalua@users.noreply.github.com>
2021-06-15 22:26:40 -0300

    att urls do detalhe

--- deliveryIngredientes/urls.py
+++ deliveryIngredientes/urls.py
@@ -3,5 +3,5 @@ from . import views
 """todas as urls referente a gestãoacademica deverão estar nesse aquivo"""
 urlpatterns = [
  path('', views.listar_receitas, name='lista-receitas'),
- path('detalhes_produto/<int:id>', views.detalhes_produto, name="detalhes"),
+ path('detalhes_produto/<int:id>', views.detalhes_produto, name="detalhes_produto"),
 ]
