a6f90d1317ae66be55c7d675e7ad90eb5e8d560c
carolalua <57256788+carolalua@users.noreply.github.com>
2021-06-15 21:02:58 -0300

    models atualização

--- deliveryIngredientes/models.py
+++ deliveryIngredientes/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-class Categoria(models.Model):
+class Ingredientes(models.Model):
+    nome = models.CharField(max_length=100, blank=True, null=True)
+    preco = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    quantidade = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
     STATUS = (
         ('proteina', 'Proteina'),
         ('frutas', 'Frutas'),
@@ -11,15 +14,7 @@ class Categoria(models.Model):
         choices=STATUS,
     )
     def __str__(self):
-        return f"{self.status}"
-    
-class Ingredientes(models.Model):
-    nome = models.CharField(max_length=100, blank=True, null=True)
-    preco = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
-    quantidade = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
-    categoria = models.ForeignKey('Categoria', verbose_name="Categoria", on_delete=models.CASCADE)
-    def __str__(self):
-        return f"{self.nome}, {self.preco}, {self.quantidade}, {self.categoria}"
+        return f"{self.nome}, {self.preco}, {self.quantidade}, {self.status}"
 class Receita(models.Model):
     nome = models.CharField(max_length=100, blank=True, null=True)
     preco = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
